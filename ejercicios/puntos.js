61f6b5e85bbc411158fe8db38df53b3f0eb38ed0
mbordon22 <67611026+mbordon22@users.noreply.github.com>
2021-04-23 19:11:22 -0300

    Ejercicios terminados

---
+++ ejercicios/puntos.js
@@ -0,0 +1,11 @@
+/* CONSIGNAS :exclamación:  Más suaves, para que (especialmente quienes tienen dudas con lo anterior) empiecen con algo más liviano.
+
+:uno: Armar una página que tenga solo un botón que, al clickearlo, muestre por consola "¡HOLA MUNDO!"
+
+:dos: Página con un input tipo texto (type="text") y un botón. Al clickear el botón, mostrar por consola el valor del texto ingresado.
+
+:tres: Página con un input tipo password (type="password"), un botón "revelar" y un elemento <p>. Al clickear el botón, cambiar el textContent del elemento <p> para que muestre el texto ingresado en el input (que se accede con su value igual que en un input text, lo único que cambia es que el navegador muestra pelotitas)
+
+:cuatro: Página con dos botones, que se muestren como "Botón 1" y "Botón 2", que al clickear cada uno muestre por consola un mensaje indicando cuál se clickeó.
+
+:cinco: Página con un input numérico (type="number") y dos botones, uno muestra por consola el valor del número y el otro muestra el valor multiplicado por 2. */
